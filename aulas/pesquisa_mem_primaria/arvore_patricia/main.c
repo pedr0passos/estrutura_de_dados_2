df6b91e26a886aa26b1f80111f3df402250ecf23
Pedro Passos <pedro.passos@hotmail.com>
2022-10-09 20:51:39 -0300

    Update main.c

--- aulas/pesquisa_mem_primaria/arvore_patricia/main.c
+++ aulas/pesquisa_mem_primaria/arvore_patricia/main.c
@@ -1,50 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-typedef struct patricinha {
-    int tipo_no;
-    int posicao;
-    char chave[8];
-    struct noPatricia *esquerda;
-    struct noPatricia *direita;
-}no_pat;
+// typedef struct patricinha {
+//     int tipo_no;
+//     int posicao;
+//     char chave[8];
+//     struct noPatricia *esquerda;
+//     struct noPatricia *direita;
+// }no_pat;
 
-no_pat inserir (no_pat *patricia, char chave_1[8], no_pat intermediario_retorna) {
-    no_pat *novo = malloc(sizeof(no_pat)), *intermediario;
-    novo->chave = chave_1;
-    novo->tipo_no = 0;
+// no_pat inserir (no_pat *patricia, char chave_1[8], no_pat intermediario_retorna) {
+//     no_pat *novo = malloc(sizeof(no_pat)), *intermediario;
+//     novo->chave = chave_1;
+//     novo->tipo_no = 0;
 
-    if ( (patricia) == NULL ) {
+//     if ( (patricia) == NULL ) {
 
-        novo->posicao = 0;
-        (patricia) = novo;
+//         novo->posicao = 0;
+//         (patricia) = novo;
 
-    } else {
+//     } else {
 
-        int n = 0;
-        if ((*patricia)->tipo_no == 0 ) {
-            while ( n < 8 && patricia->chave[n] == chave_1[n] ) {
-                n++;
-            }
-            if ( n < 8 ) {
+//         int n = 0;
+//         if ((*patricia)->tipo_no == 0 ) {
+//             while ( n < 8 && patricia->chave[n] == chave_1[n] ) {
+//                 n++;
+//             }
+//             if ( n < 8 ) {
 
-                novo->tipo_no = 0;
-                novo->esquerda = NULL;
-                novo->direita = NULL;
+//                 novo->tipo_no = 0;
+//                 novo->esquerda = NULL;
+//                 novo->direita = NULL;
 
-                intermediario = malloc(sizeof(no_pat));
-                intermediario->tipo_no = 1;
-                intermediario->posicao = n;
+//                 intermediario = malloc(sizeof(no_pat));
+//                 intermediario->tipo_no = 1;
+//                 intermediario->posicao = n;
 
-                if ( chave_1[n] == 1 ) {
-                    intermediario->direita = novo;
-                } else {
-                    intermediario->esquerda = novo;
-                }
+//                 if ( chave_1[n] == 1 ) {
+//                     intermediario->direita = novo;
+//                 } else {
+//                     intermediario->esquerda = novo;
+//                 }
 
-            }
-        }
+//             }
+//         }
 
 
-    }
-}
+//     }
+// }
