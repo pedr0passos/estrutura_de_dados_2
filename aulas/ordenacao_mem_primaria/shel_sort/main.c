ae7eb28ee29a26b3b960cce648e260cbfef2d942
Pedro Passos <pedro.passos@hotmail.com>
2022-10-15 14:36:10 -0300

    Create main.c

---
+++ aulas/ordenacao_mem_primaria/shel_sort/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define N 6
+
+
+// h = h*3 + 1 
+
+int d(int h) {
+     return (h = (h-1)/3);
+}
+
+void shellsort(int v[], int n) {
+
+    int i, j, h, auxiliar;
+    
+    h = 1; // iniciando o h como 1, o h é a quantidade de casas que se deve pular para fazer as comparações
+
+    //laço que calcula o valor de h
+    while (h < n) {
+        h = h*3 + 1;
+    }
+
+    // laço principal 
+    while ( h > 1 ) {
+        h = d(h); // h = 4
+        for (i = h; i < n; i++) {
+            auxiliar = v[i];
+            j = i - h;
+            while ( j >= 0 && auxiliar < v[j] ) {
+                v[j + h] = v[j];
+                j -= h;
+            }
+            v[j+h] = auxiliar;
+        }
+    }
+}
+
+int main () {
+
+    // vetor que vai ser ordenado
+    int vetor[N] = {23,5,7,1,98,34};
+    for ( int i = 0; i < N; i++ ) {
+            printf("%d ", vetor[i]);
+    }
+    printf("\n");
+    shellsort(vetor, N);
+    for ( int i = 0; i < N; i++ ) {
+            printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
